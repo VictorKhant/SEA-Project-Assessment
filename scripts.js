4a2ac1f0bb8276a0f8987ae8d2896d92d4a3e196
VictorKhant <mkhant@go.pasadena.edu>
2024-04-08 17:05:57 -0700

    fixed searchbar and filter

--- scripts.js
+++ scripts.js
@@ -25,10 +25,7 @@
 // Importing data of video games
 import videoGames from "./data.js";
 let filteredGames = [];
-let sort_genre = "";
-let sort_order = "";
-let is_searched = false;
-let last_platform = "ALL";
+
 // This is an array of strings (Video Games objects)
 
 // Your final submission should have much more data than this, and
@@ -36,18 +33,24 @@ let last_platform = "ALL";
 
 // This function adds cards the page to display the data in the array
 
-function showCards(games = videoGames) {
+function showCards() {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
-
   const templateCard = document.querySelector(".card");
 
-  //const user_input = document.querySelector("#pokeSearch");
-  //const user_input_trimmed = user_input.value.trim;
-  for (let i = 0; i < games.length; i++) {
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let title = games[i];
+  const clicked_filter = document.querySelector(".filter");
+  const filter_type = clicked_filter.querySelector(".clicked").textContent;
+  filteredGames = filterCards(videoGames, filter_type);
+
+  const clicked_sort = document.querySelector(".sort");
+  const sort_type = clicked_sort.querySelector(".clicked").textContent;
+  filteredGames = sortCards(filteredGames, sort_type);
+
+  const searchText = document.getElementById("search-input").value;
+  filteredGames = searchCards(filteredGames, searchText);
+
+  for (let i = 0; i < filteredGames.length; i++) {
+    let title = filteredGames[i];
     const nextCard = templateCard.cloneNode(true); // Copy the template card
     editCardContent(nextCard, title); // Edit title and image
 
@@ -77,100 +80,32 @@ function editCardContent(card, newTitle) {
 
 
 
-window.quoteAlert = function () {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
-};
+// filtering games by platform
+function filterCards(gameArr, clicked)
+{
+  if(clicked =="ALL")
+    return gameArr;
+  return gameArr.filter(game =>game.platform.toLowerCase().includes(clicked.toLowerCase()));
+}
 
-window.removeLastCard = function () {
-  videoGames.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-};
+// sorting game by rating or release date
+function sortCards(gameArr, clicked)
+{
+  const splittedStr=clicked.split(" "); 
+  if(splittedStr[0].toLowerCase()== "rating")
+    gameArr.sort((a,b)=>parseFloat(a.user_review) - parseFloat(b.user_review));
+  else
+    gameArr.sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
+  if(splittedStr[1]=="high-low" ||splittedStr[1]=="new-old")
+    gameArr.reverse();
+  return gameArr;
+}
 
-window.filteredCard = function (platform) {
-  let temp = [];
-  if(last_platform!="ALL" || platform=="ALL"){
-    document.getElementById("search-input").value = "";
-    is_searched = false;
-  }
-  
-  last_platform=platform;
-  
-  if (!is_searched) {
-    temp = videoGames;
-  } else {
-    temp = filteredGames;
-  }
-  filteredGames = [];
- 
-
-  if (platform == "ALL") {
-    return showCards();
-  }
-
-  for (let i = 0; i < temp.length; i++) {
-    if (temp[i].platform.includes(platform)) {
-      filteredGames.push(temp[i]);
-    }
-  }
-
-  if (sort_genre != "" && filteredGames.length!=0) return sortedCard(sort_genre, sort_order);
-
-  return showCards(filteredGames);
-};
-
-window.sortedCard = function (category = "release", order = "descend") {
-  sort_genre = category;
-  sort_order = order;
-
-  if (filteredGames.length == 0) {
-    filteredGames = videoGames;
-  }
-
-  if (category == "rating") {
-    filteredGames.sort(
-      (a, b) => parseFloat(a.user_review) - parseFloat(b.user_review)
-    );
-  } else if (category == "release") {
-    filteredGames.sort(
-      (a, b) => new Date(a.release_date) - new Date(b.release_date)
-    );
-  }
-  if (order == "descend") filteredGames.reverse();
-  return showCards(filteredGames);
-};
-
-// Function to perform search
-function searchGames() {
-  if (filteredGames.length == 0) filteredGames = videoGames;
-  const searchText = document.getElementById("search-input").value.toLowerCase();
-
-  const buttons = document.querySelectorAll(".filter button");
-
-  // Reset all buttons to a default color
-  buttons.forEach(
-    (button) => {
-      button.style.backgroundColor = "snow";
-    }
-  );
-  last_platform="ALL";
-
-  // Check if the search text is empty
-  if (searchText.length == 0) {
-    is_searched = false;
-    filteredGames = videoGames;
-    if (sort_genre != "") return sortedCard(sort_genre, sort_order);
-    return showCards(videoGames); // Display all games if the search is cleared
-  }
-
-  const searchedGames = filteredGames.filter((game) =>
-    game.name.toLowerCase().includes(searchText)
-  );
-  filteredGames = searchedGames;
-  is_searched = true;
-  return showCards(filteredGames);
+// searching game by user input on the search bar
+function searchCards(gameArr, user_input){
+  if (user_input.length==0)
+  return gameArr;
+  return gameArr.filter((game) =>game.name.toLowerCase().includes(user_input.toLowerCase()));
 }
 
 // Function to change the background color of the clicked button and reset others
@@ -205,24 +140,27 @@ function changeSortColor(clickedButton) {
 document.addEventListener("DOMContentLoaded", showCards());
 
 // Detecting input in search box
-document.getElementById("search-input").addEventListener("input", searchGames);
+document.getElementById("search-input").addEventListener("input",function() {
+  showCards()});
 
-// changing color of filter buttons on click
-document.addEventListener("DOMContentLoaded", () => {
-  const filterButtons = document.querySelectorAll(".filter button");
-  filterButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      changeFilterColor(this); // changing color of click button
-    });
+//detecting filter button clicked
+let filterbtns = document.querySelectorAll(".filter button");
+filterbtns.forEach(function(button) {
+  button.addEventListener('click', function() {
+      document.querySelector('.filter .clicked').classList.remove('clicked');
+      button.classList.add('clicked');
+      changeFilterColor(this);
+      showCards(); // Call showCards again to refresh
   });
 });
 
-//changing color of sort buttons
-document.addEventListener("DOMContentLoaded", () => {
-  const filterButtons = document.querySelectorAll(".sort button");
-  filterButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      changeSortColor(this); // changing color of click button
-    });
+//detecting sort button clicked
+let sortbtns = document.querySelectorAll(".sort button");
+sortbtns.forEach(function(button) {
+  button.addEventListener('click', function() {
+      document.querySelector('.sort .clicked').classList.remove('clicked');
+      button.classList.add('clicked');;
+      changeSortColor(this);
+      showCards(); // Call showCards again to refresh
   });
 });
